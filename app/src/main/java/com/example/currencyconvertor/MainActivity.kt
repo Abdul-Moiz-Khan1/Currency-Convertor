cc59ce5f9d819da00456113efa4ad98f1d5bd55a
Abdul Moiz Khan <136478539+Abdul-Moiz-Khan1@users.noreply.github.com>
2024-05-06 15:10:13 +0500

    Under Development

--- app/src/main/java/com/example/currencyconvertor/MainActivity.kt
+++ app/src/main/java/com/example/currencyconvertor/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.currencyconvertor.databinding.ActivityMainBinding
-import okhttp3.OkHttpClient
+
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Retrofit
@@ -14,7 +14,8 @@ import kotlin.time.times
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var okHttpClient: OkHttpClient
+    private lateinit var apiInterface: ApiInterface
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,29 +29,25 @@ class MainActivity : AppCompatActivity() {
         var rate = 1
 
 
-        okHttpClient = OkHttpClient.Builder()
-            .build()
-
-
-
         binding.convertBtn.setOnClickListener {
             from = binding.to.text.toString()
-            to  = binding.from.text.toString()
+            to = binding.from.text.toString()
 
-//            val am = amount.toInt()
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://www.alphavantage.co/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build().create(ApiInterface::class.java)
-            val response = retrofit.getrates("CURRENCY_EXCHANGE_RATE",from, to, "3LONZJDNI5HT7MGD")
+            val response = retrofit.getrates("CURRENCY_EXCHANGE_RATE", from, to, "3LONZJDNI5HT7MGD")
             response.enqueue(object : Callback<Response> {
                 override fun onResponse(p0: Call<Response>, p1: retrofit2.Response<Response>) {
                     val responseBody = p1.body()
                     if (p1.isSuccessful && responseBody != null) {
-                        exchangeRate = responseBody.RealtimeCurrencyExchange?.ExchangeRate.toString()
+                        exchangeRate =
+                            responseBody.RealtimeCurrencyExchange?.ExchangeRate.toString()
                         if (exchangeRate != null) {
                             Log.d("Taggg", exchangeRate)
                             binding.conversionRate.text = exchangeRate
+                            showconversion(exchangeRate)
                         } else {
                             Log.d("Taggg", "Exchange rate is null")
                         }
@@ -68,13 +65,22 @@ class MainActivity : AppCompatActivity() {
 
             })
         }
-        binding.tempbtn.setOnClickListener {
-            rate = exchangeRate.toInt()
-            var res = rate * 1000
-            binding.result.text = res.toString()
-        }
 
 
 
+    }
+
+    private fun showconversion(rate:String) {
+        val ratee = rate.toDouble()
+        val ammm = binding.amount.toString()
+        val temp = ammm.toDouble()
+
+        binding.result.text = temp.toString()
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        // Release resources
+        // (Retrofit instance doesn't require explicit shutdown)
     }
 }
